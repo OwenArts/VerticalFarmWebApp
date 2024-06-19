e8b0cecc54fef2f15f5cb0162fd2af6f8a518181
Owen Arts <owen.arts@hotmail.com>
2024-06-19 15:55:43 +0200

    update on color

--- src/Pages/NoMatch.tsx
+++ src/Pages/NoMatch.tsx
@@ -19,7 +19,7 @@ export default function NoMatch() {
     return (
         <main className="grid w-screen min-h-screen place-items-center bg-leaves-background bg-no-repeat bg-cover px-6 py-24 sm:py-32 lg:px-8">
             <div className="text-center bg-base-300 rounded-md p-24">
-                <p className="text-accent font-bold text-2xl">404</p>
+                <p className="text-red-400 font-bold text-2xl">404</p>
                 <h1 className="mt-4 text-3xl font-bold tracking-tight text-base-content sm:text-5xl">Page not found</h1>
                 <p className="mt-6 text-base leading-7 text-base-content">Sorry, we couldn’t find the page you’re looking for.</p>
                 <div className="mt-10 flex items-center justify-center gap-x-6">
